a00a1fd97c771e0a336b73bf7129b79722e6342f
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-03-31 19:08:21 +0700

    final touch

--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -1,23 +1,26 @@
 <template>
   <div>
     <form id="form" @submit.prevent="handleSubmit">
-      <label> 
+      <label>
         McName
         <input type="text" v-model="title" />
       </label>
 
       <label>
         McPicture
-        <input type="url" v-model="imageUrl" placeholder="https://"/>
+        <input type="url" v-model="imageUrl" placeholder="https://" />
       </label>
 
       <label class="grid2Col">
         McAbout
-        <textarea v-model="description" placeholder="Sharing is caring, tell us more about you"></textarea>
+        <textarea
+          v-model="description"
+          placeholder="Sharing is caring, tell us more about your nugget"
+        ></textarea>
       </label>
 
       <div class="grid2Col" style="text-align: center">
-        <button type="submit" class="button">SUBMIT</button> 
+        <button type="submit" class="button">SUBMIT</button>
       </div>
     </form>
   </div>
@@ -38,24 +41,27 @@ export default {
 
   methods: {
     handleSubmit() {
-      console.log("new chicken nugget form submitted")
+      console.log("new chicken nugget form submitted");
 
       //create a new object
-      let newForm = {title: this.title, imageUrl: this.imageUrl, description: this.description};
-      //EVENT= send the createproject and object to the parent  
+      let newForm = {
+        title: this.title,
+        imageUrl: this.imageUrl,
+        description: this.description,
+      };
+      //EVENT= send the createproject and object to the parent
       this.$emit("createProject", newForm);
 
       // reset the form
-      this.title = '';
-      this.imageUrl = '';
-      this.description = '';
-    }
-  }
+      this.title = "";
+      this.imageUrl = "";
+      this.description = "";
+    },
+  },
 };
 </script>
 
 <style>
-
 form {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
@@ -63,10 +69,10 @@ form {
   font-family: helvetica;
   margin: 20px 0px;
   padding: 0px 30px;
-  
 }
 
-input, textarea {
+input,
+textarea {
   display: block;
   width: 100%;
   margin: 10px 0px;
@@ -75,7 +81,6 @@ input, textarea {
   padding: 5px;
   font-family: helvetica;
 }
-
 
 .grid2Col {
   grid-column: 1 / span 2;
@@ -97,7 +102,7 @@ input, textarea {
 
 #form button:hover {
   color: #ffbc0d;
-  background-color: #DA291C;
+  background-color: #da291c;
 }
 
 /* https://images.bauerhosting.com/legacy/lifestyle-legacy/97/65414/90b1f/f0375/58909/1f28a/f2086/IMG_0496_640x640.jpg?q=80&w=440 */
--- src/components/ThumbGrid.vue
+++ src/components/ThumbGrid.vue
@@ -1,36 +1,37 @@
 <template>
-    <div id="userClass">
-      <h2>McShapes</h2>
-      <div class="thumbGrid">
-        <!-- v-for = for each image in my whole array of objet -->
-        <img class="thumbGrid" v-for="i in minion2"
+  <div id="userClass">
+    <h2>McShapes</h2>
+    <div class="thumbGrid">
+      <!-- v-for = for each image in my whole array of objet -->
+      <img
+        class="thumbGrid"
+        v-for="i in minion2"
         :key="i.id"
         :src="i.imageUrl"
         :alt="i.title"
         @click="handleClick(i.id)"
-        />
-      </div>
+      />
     </div>
+  </div>
 </template>
 
-  <script>
-  export default {
-    name: "ThumbGrid",
-    
-    emits: ["shareId2"],
-    props: ["minion2"],
+<script>
+export default {
+  name: "ThumbGrid",
 
-    methods: {
-      handleClick(id) {
-        this.$emit("shareId2", id);
-        console.log("McDonald! McNugget! McLoving it!!!!!");
-      },
-    }
+  emits: ["shareId2"],
+  props: ["minion2"],
 
-  };
-  </script>
-  
-  <style>
+  methods: {
+    handleClick(id) {
+      this.$emit("shareId2", id);
+      console.log("McDonald! McNugget! McLoving it!!!!!");
+    },
+  },
+};
+</script>
+
+<style>
 #userClass .thumbGrid {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -46,11 +47,9 @@
   aspect-ratio: 1 / 1;
   cursor: pointer;
   border: 1px solid #ffbc0d;
-
 }
 
 #userClass .thumbGrid img:hover {
   border: 1px solid #292929;
 }
-
-  </style>
+</style>
